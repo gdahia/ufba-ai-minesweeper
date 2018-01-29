1fc2e6b1b869868ba99439f405be0de1de761678
Gabriel Dahia <gdahia@gmail.com>
2018-01-29 10:50:39 -0300

    add untested logical player

--- players/logical.py
+++ players/logical.py
@@ -1,11 +1,13 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+from six.moves import range
 
 import numpy as np
 
 
 class LogicalPlayer:
+
   def __init__(self, no_move_strategy=None):
     self._sure_moves = []
 
@@ -18,8 +20,8 @@ class LogicalPlayer:
     pass
 
   def strategy(self, board):
-    # se ainda movimentos no buffer, executa o proximo desses movimentos
-    if self._sure_bombs:
+    # se ainda ha movimentos no buffer, executa o proximo desses movimentos
+    if self._sure_moves:
       move = self._sure_moves[0]
       self._sure_moves = self._sure_moves[1:]
       return move
@@ -46,5 +48,62 @@ class LogicalPlayer:
     return 'C', row, col
 
   def _get_sure_moves(self, board):
-    # todo
-    pass
+    # determina posicoes na borda
+    border_list, insiders_list = self._get_border_and_insiders(board)
+
+    # salva os movimentos de modelos que atendem o tabuleiro
+    current_moves = np.ones(len(border_list), dtype=np.bool)
+
+    # copia o tabuleiro para fazer computacoes
+    safe_board = np.zeros(board, dtype=np.uint8)
+
+    # itera sobre possibilidades
+    for possibility in range(2**len(border_list)):
+      # preenche a copia do tabuleiro de acordo com 'possibility'
+      for index, border_pos in enumerate(border_list):
+        # ve se 'possibility' preve bomba na posicao 'border_pos'
+        has_bomb = bool(possibility & (1 << index))
+
+        if has_bomb:
+          safe_board[border_pos] = 1
+        else:
+          safe_board[border_pos] = 0
+
+      # checa se modelo gerado e possivel
+      if self._check_model_veracity(safe_board, insiders_list):
+        # itera sobre as posicoes de borda e salva movimentos determinados por 'possibility'
+        for index, border_pos in enumerate(border_list):
+          current_moves[index] |= not safe_board[border_pos]
+
+    return border_list[current_moves]
+
+  def _get_border_and_insiders(self, board):
+    border = []
+    insiders = []
+    for i, row in enumerate(1, board):
+      for j, pos in enumerate(1, row):
+        # checa se posicao esta fechada
+        if pos != -1:
+          # adiciona posicao aberta aos vizinhos da borda
+          insiders.append((i, j))
+
+          # itera sobre vizinhanca de posicao fechada buscando posicoes abertas
+          for di in range(-1, 2):
+            for dj in range(-1, 2):
+              if board[i + di, j + dj] == -1:
+                border.append((i + di, j + dj))
+
+    # remove elementos repetidos
+    border = np.array(border, dtype=np.int32)
+    border = np.unique(border)
+
+    return border, insiders
+
+  def _check_model_veracity(self, board, safe_board, insiders):
+    # itera sobre posicoes abertas vizinhas a borda
+    for i, j in insiders:
+      # checa se modelo atende restricoes de posicao aberta '(i, j)'
+      if np.sum(safe_board[i - 1:i + 2, j - 1:j + 2]) != board[i, j]:
+        return False
+
+    return True
